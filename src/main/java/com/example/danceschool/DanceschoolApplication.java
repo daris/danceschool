7860dc069d6fc8be2658713d3cea817a6d533f9b
daris <daris91@gmail.com>
2025-11-07 14:18:28 +0100

    initial app

---
+++ src/main/java/com/example/danceschool/DanceschoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.danceschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceschoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanceschoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/danceschool/DanceschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.danceschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanceschoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
