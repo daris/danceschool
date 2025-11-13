2052b197cfcf392605fd28f3c120920f639e2685
daris <daris91@gmail.com>
2025-11-13 12:10:02 +0100

    openapi config to allow setting authorization header

--- src/main/java/com/example/danceschool/model/projection/UserWithPasses.java
+++ src/main/java/com/example/danceschool/model/projection/UserWithPasses.java
@@ -1,12 +1,13 @@
 package com.example.danceschool.model.projection;
 
 import com.example.danceschool.model.Course;
+import com.example.danceschool.model.User;
 import org.springframework.data.rest.core.config.Projection;
 
 import java.util.List;
 import java.util.UUID;
 
-@Projection(name = "withPasses", types = Course.class)
+@Projection(name = "withPasses", types = User.class)
 public interface UserWithPasses {
     UUID getId();
     String getFirstName();
