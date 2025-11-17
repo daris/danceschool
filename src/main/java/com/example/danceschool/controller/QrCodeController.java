f2451a0574f7a9e6047ad84407aa8e34894313ff
daris <daris91@gmail.com>
2025-11-17 19:08:54 +0100

    scan qr: add participant to lesson when he/she is not already participating

--- src/main/java/com/example/danceschool/controller/QrCodeController.java
+++ src/main/java/com/example/danceschool/controller/QrCodeController.java
@@ -3,9 +3,9 @@ package com.example.danceschool.controller;
 import com.example.danceschool.component.SecurityUtils;
 import com.example.danceschool.dto.ErrorResponse;
 import com.example.danceschool.dto.QrCodeRequest;
-import com.example.danceschool.model.AttendanceStatus;
-import com.example.danceschool.model.Lesson;
+import com.example.danceschool.model.*;
 import com.example.danceschool.repository.LessonRepository;
+import com.example.danceschool.repository.ParticipantRepository;
 import com.example.danceschool.service.AttendanceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,21 +20,35 @@ public class QrCodeController {
     private final LessonRepository lessonRepository;
     private final AttendanceService attendanceService;
     private final SecurityUtils securityUtils;
+    private final ParticipantRepository participantRepository;
 
-    public QrCodeController(LessonRepository lessonRepository, AttendanceService attendanceService, SecurityUtils securityUtils) {
+    public QrCodeController(LessonRepository lessonRepository, AttendanceService attendanceService, SecurityUtils securityUtils, ParticipantRepository participantRepository) {
         this.lessonRepository = lessonRepository;
         this.attendanceService = attendanceService;
         this.securityUtils = securityUtils;
+        this.participantRepository = participantRepository;
     }
 
     @PostMapping("/qr")
     public ResponseEntity<?> qr(@RequestBody QrCodeRequest qrCodeRequest) {
-        Optional<Lesson> lesson = lessonRepository.findById(qrCodeRequest.getId());
-        if (lesson.isEmpty()) {
+        Optional<Lesson> lessonOptional = lessonRepository.findById(qrCodeRequest.getId());
+        if (lessonOptional.isEmpty()) {
             return new ResponseEntity<>(new ErrorResponse("Lesson not found"), HttpStatus.NOT_FOUND);
         }
 
-        attendanceService.setAttendanceStatusForLesson(lesson.get(), securityUtils.getCurrentUser(), AttendanceStatus.NORMAL);
+        Lesson lesson = lessonOptional.get();
+        User user = securityUtils.getCurrentUser();
+
+        Course course = lesson.getCourse();
+        boolean isParticipating = course.getParticipants().stream().anyMatch(participant -> participant.getUser().getId().equals(user.getId()));
+        if (!isParticipating) {
+            Participant participant = new Participant();
+            participant.setUser(user);
+            participant.setCourse(course);
+            participantRepository.save(participant);
+        }
+
+        attendanceService.setAttendanceStatusForLesson(lesson, user, AttendanceStatus.NORMAL);
 
         return ResponseEntity.ok().body("");
     }
