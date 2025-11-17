a8e271fd5c38f4a507e8eb6c7280ac641dda6050
daris <daris91@gmail.com>
2025-11-17 18:33:23 +0100

    fix loading current user from auth context

--- src/main/java/com/example/danceschool/component/SecurityUtils.java
+++ src/main/java/com/example/danceschool/component/SecurityUtils.java
@@ -5,6 +5,7 @@ import com.example.danceschool.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,13 +15,18 @@ public class SecurityUtils {
     private UserRepository userRepository;
 
     public User getCurrentUser() {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth == null) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
             throw new RuntimeException("Unauthorized");
         }
 
-        var userDetails = (org.springframework.security.core.userdetails.User) auth.getPrincipal();
-        return userRepository.findByUsername(userDetails.getUsername())
-                .orElseThrow(() -> new RuntimeException("User not found"));
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof User user) {
+            return user;
+        } else if (principal instanceof UserDetails userDetails) {
+            return userRepository.findByUsername(userDetails.getUsername())
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+        }
+        return null;
     }
 }
