d8815e8f467da0a390f1b4896ba57550d5c019f0
daris <daris91@gmail.com>
2025-11-22 09:38:13 +0100

    cleanup controllers, move logic to services

--- src/main/java/com/example/danceschool/controller/AttendanceController.java
+++ src/main/java/com/example/danceschool/controller/AttendanceController.java
@@ -1,17 +1,15 @@
 package com.example.danceschool.controller;
 
-import com.example.danceschool.dto.*;
+import com.example.danceschool.dto.AttendanceStatusRequest;
 import com.example.danceschool.model.Attendance;
 import com.example.danceschool.model.Lesson;
 import com.example.danceschool.model.User;
 import com.example.danceschool.model.projection.AttendanceExcerpt;
-import com.example.danceschool.service.AttendanceService;
 import com.example.danceschool.service.CourseService;
 import com.example.danceschool.service.LessonService;
 import com.example.danceschool.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +22,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/attendances")
 public class AttendanceController {
     private final LessonService lessonService;
-    private final AttendanceService attendanceService;
     private final CourseService courseService;
     private final UserService userService;
     private final ProjectionFactory projectionFactory;
@@ -34,8 +31,7 @@ public class AttendanceController {
         Lesson lesson = lessonService.getById(qrCodeRequest.getLessonId());
         User user = userService.getById(qrCodeRequest.getUserId());
 
-        Attendance attendance = attendanceService.setAttendanceStatusForLesson(lesson, user, qrCodeRequest.getStatus());
-        courseService.notifyCourseAttendancesChanged(lesson, user, attendance);
+        Attendance attendance = courseService.setAttendanceStatusForLesson(lesson, user, qrCodeRequest.getStatus());
 
         AttendanceExcerpt attendanceExcerpt = projectionFactory.createProjection(AttendanceExcerpt.class, attendance);
         return ResponseEntity.ok().body(attendanceExcerpt);
--- src/main/java/com/example/danceschool/service/CourseService.java
+++ src/main/java/com/example/danceschool/service/CourseService.java
@@ -4,7 +4,6 @@ import com.example.danceschool.dto.CourseAttendancesUpdateDto;
 import com.example.danceschool.model.*;
 import com.example.danceschool.repository.ParticipantRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +14,21 @@ public class CourseService {
     private final AttendanceService attendanceService;
     private final SimpMessagingTemplate messagingTemplate;
 
-    public void createParticipantForCourseIfNotAlready(Course course, User user) {
+    public void registerAttendanceForCourse(Course course, Lesson lesson, User user) {
+        createParticipantForCourseIfNotAlready(course, user);
+        Attendance attendance = attendanceService.setAttendanceStatusForLesson(lesson, user, AttendanceStatus.NORMAL);
+
+        notifyCourseAttendancesChanged(lesson, user, attendance);
+    }
+
+    public Attendance setAttendanceStatusForLesson(Lesson lesson, User user, AttendanceStatus status) {
+        Attendance attendance = attendanceService.setAttendanceStatusForLesson(lesson, user, status);
+        notifyCourseAttendancesChanged(lesson, user, attendance);
+
+        return attendance;
+    }
+
+    private void createParticipantForCourseIfNotAlready(Course course, User user) {
         boolean isParticipating = course.getParticipants().stream().anyMatch(participant -> participant.getUser().getId().equals(user.getId()));
         if (!isParticipating) {
             Participant participant = new Participant();
@@ -25,14 +38,7 @@ public class CourseService {
         }
     }
 
-    public void registerAttendanceForCourse(Course course, Lesson lesson, User user) {
-        createParticipantForCourseIfNotAlready(course, user);
-        Attendance attendance = attendanceService.setAttendanceStatusForLesson(lesson, user, AttendanceStatus.NORMAL);
-
-        notifyCourseAttendancesChanged(lesson, user, attendance);
-    }
-
-    public void notifyCourseAttendancesChanged(Lesson lesson, User user, Attendance attendance) {
+    private void notifyCourseAttendancesChanged(Lesson lesson, User user, Attendance attendance) {
         CourseAttendancesUpdateDto courseAttendancesUpdateDto = new CourseAttendancesUpdateDto();
         courseAttendancesUpdateDto.setCourseId(lesson.getCourse().getId());
         courseAttendancesUpdateDto.setAttendanceId(attendance.getId());
@@ -41,9 +47,6 @@ public class CourseService {
         courseAttendancesUpdateDto.setStatus(attendance.getStatus());
 
         String topic = "/topic/courses/" + lesson.getCourse().getId() + "/attendances";
-        messagingTemplate.convertAndSend(
-                topic,
-                courseAttendancesUpdateDto
-        );
+        messagingTemplate.convertAndSend(topic, courseAttendancesUpdateDto);
     }
 }
