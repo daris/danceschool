215cdd2065bc3614132630b13b0e99253d3d6789
daris <daris91@gmail.com>
2025-12-10 09:53:29 +0100

    use spring embedded kafka for tests

--- src/test/java/com/example/danceschool/controller/AuthControllerTest.java
+++ src/test/java/com/example/danceschool/controller/AuthControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
+@EmbeddedKafka(partitions = 1, topics = { "user-created" })
 class AuthControllerTest {
 
     @Autowired
